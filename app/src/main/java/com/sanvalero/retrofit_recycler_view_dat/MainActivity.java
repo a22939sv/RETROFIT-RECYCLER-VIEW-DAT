d8366351571861c3895792f1d51e2e457ca459d1
a22939sv <a22939@svalero.com>
2023-02-16 08:17:53 +0100

    Updated

---
+++ app/src/main/java/com/sanvalero/retrofit_recycler_view_dat/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sanvalero.retrofit_recycler_view_dat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
