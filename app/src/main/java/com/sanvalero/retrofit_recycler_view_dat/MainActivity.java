826e97cb84b0891df39a4831038d00f2067dde2e
a22939sv <a22939@svalero.com>
2023-02-16 09:00:38 +0100

    Update

--- app/src/main/java/com/sanvalero/retrofit_recycler_view_dat/MainActivity.java
+++ app/src/main/java/com/sanvalero/retrofit_recycler_view_dat/MainActivity.java
@@ -2,7 +2,11 @@ package com.sanvalero.retrofit_recycler_view_dat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.sanvalero.retrofit_recycler_view_dat.view.RvProductsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+        // SetTimeOut - setInterval
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent screenChanger = new Intent(getBaseContext(),
+                        RvProductsActivity.class
+                );
+                startActivity(screenChanger);
+            }
+        }, 2000);
     }
 }
